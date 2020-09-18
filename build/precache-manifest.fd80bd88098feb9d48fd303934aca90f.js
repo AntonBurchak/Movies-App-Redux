7a6d43525c033f5f822d8f27d830af17da9ee09b
Anton Burchak <anton.burchak@gmail.com>
2020-09-18 21:14:00 +0300

    finish

---
+++ build/precache-manifest.fd80bd88098feb9d48fd303934aca90f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d13f7fddcd79588310a6cd71f70fae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "745c82c6ee88cfb41fbf",
+    "url": "/static/css/2.a29ded2a.chunk.css"
+  },
+  {
+    "revision": "c79408433b1d73746f01",
+    "url": "/static/css/main.edf97702.chunk.css"
+  },
+  {
+    "revision": "745c82c6ee88cfb41fbf",
+    "url": "/static/js/2.3b101a4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b101a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79408433b1d73746f01",
+    "url": "/static/js/main.2ff49a74.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
